c55552b6ad47c6915b1b395bd143cadcf68bee48
Vivek Mishra <vivekhere01@gmail.com>
2024-04-05 21:07:24 +0530

    add extra options

--- context/socket.js
+++ context/socket.js
@@ -4,24 +4,26 @@ import { io } from "socket.io-client";
 const SocketContext = createContext(null);
 
 export const useSocket = () => {
-    const socket = useContext(SocketContext)
-    return socket
-}
+  const socket = useContext(SocketContext);
+  return socket;
+};
 
 export const SocketProvider = (props) => {
   const { children } = props;
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const connection = io();
-    console.log("socket connection", connection)
+    const connection = io("https://google-meet-nine.vercel.app", {
+      withCredentials: true,
+    });
+    console.log("socket connection", connection);
     setSocket(connection);
   }, []);
 
-  socket?.on('connect_error', async (err) => {
-    console.log("Error establishing socket", err)
-    await fetch('/api/socket')
-  })
+  socket?.on("connect_error", async (err) => {
+    console.log("Error establishing socket", err);
+    await fetch("/api/socket");
+  });
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
